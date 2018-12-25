86d6e2446fc08e003e3f2f178eddef58307d13fa
xiaochen wu <woody@wellcent.com>
2018-12-25 21:43:29 +0800

    Update Barcode.js
    fix

--- Barcode.js
+++ Barcode.js
@@ -7,75 +7,75 @@
 
 
 import React, {
-    PropTypes,
-    Component,
+	Component
 } from 'react'
 import {
-    View,
-    requireNativeComponent,
-    NativeModules,
-    AppState,
-    Platform,
+	View,
+	requireNativeComponent,
+	NativeModules,
+	AppState,
+	Platform,
 } from 'react-native'
+
+import PropTypes from 'prop-types';
 
 const BarcodeManager = Platform.OS == 'ios' ? NativeModules.Barcode : NativeModules.CaptureModule
 
 
 export default class Barcode extends Component {
 
-    static defaultProps = {
-        barCodeTypes: Object.values(BarcodeManager.barCodeTypes),
-        scannerRectWidth: 255,
-        scannerRectHeight: 255,
-        scannerRectTop: 0,
-        scannerRectLeft: 0,
-        scannerLineInterval: 3000,
-        scannerRectCornerColor: `#09BB0D`,
-    }
+	static defaultProps = {
+		barCodeTypes: Object.values(BarcodeManager.barCodeTypes),
+		scannerRectWidth: 255,
+		scannerRectHeight: 255,
+		scannerRectTop: 0,
+		scannerRectLeft: 0,
+		scannerLineInterval: 3000,
+		scannerRectCornerColor: `#09BB0D`,
+	}
 
-    static propTypes = {
-        ...View.propTypes,
-        onBarCodeRead: PropTypes.func.isRequired,
-        barCodeTypes: PropTypes.array,
-        scannerRectWidth: PropTypes.number,
-        scannerRectHeight: PropTypes.number,
-        scannerRectTop: PropTypes.number,
-        scannerRectLeft: PropTypes.number,
-        scannerLineInterval: PropTypes.number,
-        scannerRectCornerColor: PropTypes.string,
-    }
+	static propTypes = {
+		...View.propTypes,
+		onBarCodeRead: PropTypes.func.isRequired,
+		barCodeTypes: PropTypes.array,
+		scannerRectWidth: PropTypes.number,
+		scannerRectHeight: PropTypes.number,
+		scannerRectTop: PropTypes.number,
+		scannerRectLeft: PropTypes.number,
+		scannerLineInterval: PropTypes.number,
+		scannerRectCornerColor: PropTypes.string,
+	}
 
-    render() {
-        return (
-            <NativeBarCode
+	render() {
+		return (
+			<NativeBarCode
                 {...this.props}
             />
-        )
-    }
+		)
+	}
 
-    componentDidMount() {
-        AppState.addEventListener('change', this._handleAppStateChange);
-    }
-    componentWillUnmount() {
-        AppState.removeEventListener('change', this._handleAppStateChange);
-    }
+	componentDidMount() {
+		AppState.addEventListener('change', this._handleAppStateChange);
+	}
+	componentWillUnmount() {
+		AppState.removeEventListener('change', this._handleAppStateChange);
+	}
 
-    startScan() {
-        BarcodeManager.startSession()
-    }
+	startScan() {
+		BarcodeManager.startSession()
+	}
 
-    stopScan() {
-        BarcodeManager.stopSession()
-    }
+	stopScan() {
+		BarcodeManager.stopSession()
+	}
 
-    _handleAppStateChange = (currentAppState) => {
-        if(currentAppState !== 'active' ) {
-            this.stopScan()
-        }
-        else {
-            this.startScan()
-        }
-    }
+	_handleAppStateChange = (currentAppState) => {
+		if (currentAppState !== 'active') {
+			this.stopScan()
+		} else {
+			this.startScan()
+		}
+	}
 }
 
 const NativeBarCode = requireNativeComponent(Platform.OS == 'ios' ? 'RCTBarcode' : 'CaptureView', Barcode)
